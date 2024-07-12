a2526ba591bb336e684130634dd87532ec468cd5
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 20:46:46 +0700

    user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,12 @@ const express = require("express");
 //import prisma client
 const prisma = require("../prisma/client");
 
+// Import validationResult from express-validator
+const { validationResult } = require("express-validator");
+
+//import bcrypt
+const bcrypt = require("bcryptjs");
+
 //function findUsers
 const findUsers = async (req, res) => {
   try {
@@ -33,4 +39,44 @@ const findUsers = async (req, res) => {
   }
 };
 
-module.exports = { findUsers };
+//function createUser
+const createUser = async (req, res) => {
+  // Periksa hasil validasi
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    // Jika ada error, kembalikan error ke pengguna
+    return res.status(422).json({
+      success: false,
+      message: "Validation error",
+      errors: errors.array(),
+    });
+  }
+
+  //hash password
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  try {
+    //insert data
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "User created successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { findUsers, createUser };

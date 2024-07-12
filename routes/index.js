634bc08454bfd9ccb1128cd1b12ad4f2ec06c7f2
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 20:39:28 +0700

    routes user

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,17 @@ const express = require("express");
 //init express router
 const router = express.Router();
 
+//import verifyToken
+const verifyToken = require("../middlewares/auth");
+
 //import register controller
 const registerController = require("../controllers/RegisterController");
 
 //import login controller
 const loginController = require("../controllers/LoginController");
+
+//import user controller
+const userController = require("../controllers/UserController");
 
 //import validate register and login
 const { validateRegister, validateLogin } = require("../utils/validators/auth");
@@ -18,6 +24,9 @@ router.post("/register", validateRegister, registerController.register);
 
 //define route for login
 router.post("/login", validateLogin, loginController.login);
+
+//define route for user
+router.get("/admin/users", verifyToken, userController.findUsers);
 
 //export router
 module.exports = router;

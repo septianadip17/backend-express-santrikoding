75606aff9d4e96dca1d7b1233fbb300588e6c55e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 21:20:57 +0700

    update data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -111,4 +111,51 @@ const findUserById = async (req, res) => {
   }
 };
 
-module.exports = { findUsers, createUser, findUserById };
+//function updateUser
+const updateUser = async (req, res) => {
+  //get ID from params
+  const { id } = req.params;
+
+  // Periksa hasil validasi
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    // Jika ada error, kembalikan error ke pengguna
+    return res.status(422).json({
+      success: false,
+      message: "Validation error",
+      errors: errors.array(),
+    });
+  }
+
+  //hash password
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  try {
+    //update user
+    const user = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      },
+    });
+
+    //send response
+    res.status(200).send({
+      success: true,
+      message: "User updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { findUsers, createUser, findUserById, updateUser };

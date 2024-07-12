eb9b4dd811a43130eca0c814c5a9b60efccd5136
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 20:23:27 +0700

    login controller added

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,23 @@
 //import express
-const express = require('express')
+const express = require("express");
 
 //init express router
 const router = express.Router();
 
 //import register controller
-const registerController = require('../controllers/RegisterController');
+const registerController = require("../controllers/RegisterController");
 
-//import validate register
-const { validateRegister } = require('../utils/validators/auth');
+//import login controller
+const loginController = require("../controllers/LoginConroller");
+
+//import validate register and login
+const { validateRegister, validateLogin } = require("../utils/validators/auth");
 
 //define route for register
-router.post('/register', validateRegister, registerController.register);
+router.post("/register", validateRegister, registerController.register);
+
+//define route for login
+router.post("/login", validateLogin, loginController.login);
 
 //export router
-module.exports = router
+module.exports = router;

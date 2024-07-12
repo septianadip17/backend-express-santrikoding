13a7ac1564fe5c8f39b11894c91bec15c4c85c10
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 21:25:07 +0700

    delete user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -158,4 +158,36 @@ const updateUser = async (req, res) => {
   }
 };
 
-module.exports = { findUsers, createUser, findUserById, updateUser };
+//function deleteUser
+const deleteUser = async (req, res) => {
+  //get ID from params
+  const { id } = req.params;
+
+  try {
+    //delete user
+    await prisma.user.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    //send response
+    res.status(200).send({
+      success: true,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = {
+  findUsers,
+  createUser,
+  findUserById,
+  updateUser,
+  deleteUser,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,57 @@
 //import express
-const express = require('express')
+const express = require("express");
 
 //init express router
 const router = express.Router();
 
 //import verifyToken
-const verifyToken = require('../middlewares/auth');
+const verifyToken = require("../middlewares/auth");
 
 //import register controller
-const registerController = require('../controllers/RegisterController');
+const registerController = require("../controllers/RegisterController");
 
 //import login controller
-const loginController = require('../controllers/LoginController');
+const loginController = require("../controllers/LoginController");
 
 //import user controller
-const userController = require('../controllers/UserController');
+const userController = require("../controllers/UserController");
 
 //import validate register and login
-const { validateRegister, validateLogin } = require('../utils/validators/auth');
+const { validateRegister, validateLogin } = require("../utils/validators/auth");
 
 //import validate user
-const { validateUser } = require('../utils/validators/user');
+const { validateUser } = require("../utils/validators/user");
 
 //define route for register
-router.post('/register', validateRegister, registerController.register);
+router.post("/register", validateRegister, registerController.register);
 
 //define route for login
-router.post('/login', validateLogin, loginController.login);
+router.post("/login", validateLogin, loginController.login);
 
 //define route for user
-router.get('/admin/users', verifyToken, userController.findUsers);
+router.get("/admin/users", verifyToken, userController.findUsers);
 
 //define route for user create
-router.post('/admin/users', verifyToken, validateUser, userController.createUser);
+router.post(
+  "/admin/users",
+  verifyToken,
+  validateUser,
+  userController.createUser
+);
 
 //define route for user by id
-router.get('/admin/users/:id', verifyToken, userController.findUserById);
+router.get("/admin/users/:id", verifyToken, userController.findUserById);
 
 //define route for user update
-router.put('/admin/users/:id', verifyToken, validateUser, userController.updateUser);
+router.put(
+  "/admin/users/:id",
+  verifyToken,
+  validateUser,
+  userController.updateUser
+);
+
+//define route for user delete
+router.delete("/admin/users/:id", verifyToken, userController.deleteUser);
 
 //export router
-module.exports = router
+module.exports = router;
